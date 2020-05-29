56285cd985161cad0b3caba7db7f8fe1926a06d2
Giang Hoang Le <37028078+leh08@users.noreply.github.com>
2020-05-30 09:20:33 +1200

    Finish OAuth

--- server/resources/google.py
+++ server/resources/google.py
@@ -1,7 +1,7 @@
-from flask import redirect, request, session
+from flask import redirect, request
 from flask_restful import Resource
 
-from services.oauth import oauth
+from services.oauth import OAuth
 """
 Use google-api-python-client library for Google is
 easiest way. rather than OAuthlib
@@ -10,7 +10,7 @@ easiest way. rather than OAuthlib
 class GoogleLogin(Resource):
     @classmethod
     def get(cls):
-        authorization_url = oauth.get_authorization_url()
+        authorization_url = OAuth('google').get_authorization_url()
         
         return redirect(
             authorization_url,
@@ -22,10 +22,8 @@ class GoogleAuthorize(Resource):
     @classmethod
     def get(cls):
         authorization_response = request.url
-        token = oauth.get_token(authorization_response)
-        client = oauth.create_client(token)
+        token = OAuth('google').get_token(authorization_response)
+        client = OAuth('google').create_client(token)
         response = client.get('https://www.googleapis.com/oauth2/v1/userinfo')
-        email = response.json()['email']
-        session['oauth_token'][email] = token
 
         return response.json()
