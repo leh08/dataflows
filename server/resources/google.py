57f469fe106d4524c46926c2d5a2064d50f0f9bd
Giang Hoang Le <37028078+leh08@users.noreply.github.com>
2020-05-30 12:41:35 +1200

    Extend OAuth

--- server/resources/google.py
+++ server/resources/google.py
@@ -1,7 +1,9 @@
 from flask import redirect, request
 from flask_restful import Resource
 
-from services.oauth import OAuth
+from models.source import SourceModel
+from models.authorization import AuthorizationModel
+from services.oauths import oauth
 """
 Use google-api-python-client library for Google is
 easiest way. rather than OAuthlib
@@ -10,7 +12,7 @@ easiest way. rather than OAuthlib
 class GoogleLogin(Resource):
     @classmethod
     def get(cls):
-        authorization_url = OAuth('google').get_authorization_url()
+        authorization_url = oauth.get_service('google').get_authorization_url()
         
         return redirect(
             authorization_url,
@@ -22,9 +24,15 @@ class GoogleAuthorize(Resource):
     @classmethod
     def get(cls):
         authorization_response = request.url
-        google = OAuth('google')
+        google = oauth.get_service('google')
         token = google.get_token(authorization_response)
-        client = google.create_client(token)
-        response = client.get('https://www.googleapis.com/oauth2/v1/userinfo')
-
+        client = google.create_client(token, account='giang.lh0469@gmail.com')
+        response = client.get('https://www.googleapis.com/oauth2/v3/userinfo')
+        
+        data_json = response.json()
+        
+        google = SourceModel.find_by_name('Google')
+        authorization = AuthorizationModel(name=data_json['email'], credential=token, source=google)
+        authorization.save_to_db()
+        
         return response.json()
--- server/services/oauths.py
+++ server/services/oauths.py
@@ -6,6 +6,15 @@ from requests_oauthlib import OAuth2Session
 
 class OAuth:
     def __init__(
+        self,
+        token_fetcher=None,
+        token_updater=None,
+    ):
+        self.platforms = {}
+        self.token_fetcher = token_fetcher
+        self.token_updater = token_updater
+        
+    def register_service(
         self, name,
         client_id=None,
         client_secret=None,
@@ -14,40 +23,54 @@ class OAuth:
         redirect_uri=None,
         token_url=None,
         base_url=None,
-        token_updater=None,
-        token_fetcher=None
-    ):
+    ):  
         config = configs.get(name)
+
+        service = OAuthService(
+            name,
+            token_fetcher = self.token_fetcher,
+            token_updater = self.token_updater,
+            **config
+        )
+        
+        self.platforms[name] = service
+    
+    def get_service(self, name):
+        return self.platforms[name]
+    
+    
+class OAuthService:
+    def __init__(
+        self, name,
+        client_id=None,
+        client_secret=None,
+        scope=None,
+        authorization_base_url=None,
+        redirect_uri=None,
+        token_url=None,
+        base_url=None,
+        token_fetcher=None,
+        token_updater=None
+        ):
         self.name = name
         
-        self.client_id = client_id or config.get('client_id')
-        self.client_secret = client_secret or config.get('client_secret')
-        self.scope = scope or config.get('scope')
-        self.authorization_base_url = authorization_base_url or config.get('authorization_base_url')
-        self.redirect_uri = redirect_uri or config.get('redirect_uri')
-        self.token_url = token_url or config.get('token_url')
-        self.base_url = base_url or config.get('base_url')
-        self.token_updater = token_updater
-        self.token_fetcher = token_fetcher
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.scope = scope
+        self.authorization_base_url = authorization_base_url
+        self.redirect_uri = redirect_uri
+        self.token_url = token_url
+        self.base_url = base_url
         
+        self.token_fetcher = token_fetcher
+        self.token_updater = token_updater
+    
     def create_client(self, token=None, account=None):
         extra = {
             'client_id': self.client_id,
             'client_secret': self.client_secret,
         }
-        def token_updater(token, name, account=None):
-            if account is None:
-                session['oauth_token'][name] = token
-                
-            else:
-                session['oauth_token'][name][account] = token
         
-        try:
-            token_updater = partial(self.token_updater, name=self.name, account=account)
-        
-        except:  
-            token_updater = partial(token_updater, name=self.name, account=account)
-            
         if token:
             
             client = OAuth2Session(
@@ -55,14 +78,27 @@ class OAuth:
                 token=token,
                 auto_refresh_kwargs=extra,
                 auto_refresh_url=self.token_url,
-                token_updater=token_updater,
+                token_updater=self.token_updater,
             )
         else:
             if self.token_fetcher is None:
                 raise TypeError("Token must be provided.")
             
             token = self.token_fetcher(self.name, account=account)
+                    
+            try:
+                token_updater = partial(self.token_updater, name=self.name, account=account)
             
+            except:
+                def default_token_updater(token, name, account=None):
+                    if account is None:
+                        session['oauth_token'][name] = token
+                        
+                    else:
+                        session['oauth_token'][name][account] = token
+                        
+                token_updater = partial(default_token_updater, name=self.name, account=account)
+
             client = OAuth2Session(
                 self.client_id,
                 token=token,
@@ -72,7 +108,7 @@ class OAuth:
             )
 
         return client
-    
+            
     def get_authorization_url(self):
         client = OAuth2Session(
             self.client_id,
@@ -97,3 +133,6 @@ class OAuth:
                                    authorization_response=authorization_response)
 
         return token
+    
+oauth = OAuth()
+oauth.register_service('google')
