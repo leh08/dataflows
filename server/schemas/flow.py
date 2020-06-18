021c2854cfb4ebfe659077c726356078a78a2512
phdnzdeveloper <phdnzdeveloper@gmail.com>
2020-06-18 12:12:30 +1200

    Update flow.py

--- server/schemas/flow.py
+++ server/schemas/flow.py
@@ -1,7 +1,6 @@
 from schema import BaseSchema
 from marshmallow_sqlalchemy.fields import Nested
 from models.flow import FlowModel
-from schemas.source import SourceSchema
 from schemas.authorization import AuthorizationSchema
 from schemas.log import LogSchema
 
@@ -9,7 +8,6 @@ from schemas.log import LogSchema
 class FlowSchema(BaseSchema):   
     class Meta(BaseSchema.Meta):
         model = FlowModel
-        
-    source = Nested(SourceSchema, exclude=['authorizations'])
+
     authorization = Nested(AuthorizationSchema, exclude=['source_id', 'created_at'])
     logs = Nested(LogSchema, many=True)
