513cad1658e4f710e575078cc9aa9b1d65283e33
phdnzdeveloper <phdnzdeveloper@gmail.com>
2020-06-24 14:25:27 +1200

    Update app.py

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
@@ -22,7 +22,7 @@ from resources.google import GoogleLogin, GoogleAuthorize
 from services.uploads import UPLOAD_SET
 from services.apscheduler import scheduler
 
-app = Flask(__name__, static_folder='../client/build/static', template_folder="../client/build")
+app = Flask(__name__)
 # Default config
 app.config.from_pyfile("default_config.py")
 # Update new parameters in config
@@ -45,10 +45,6 @@ def handle_marshmallow_validation(err):
 @jwt.token_in_blacklist_loader
 def check_if_token_in_blacklist(decrypted_token):
     return (decrypted_token["jti"] in BLACKLIST)  # Here we blacklist particular JWTs that have been created in the past.
-
-@app.route('/')
-def index():
-    return render_template('index.html')
 
 api.add_resource(SourceList, "/sources")
 api.add_resource(Source, "/sources/<int:source_id>")
@@ -75,6 +71,7 @@ if __name__ == "__main__":
         restart_db()
     else:
         init_db()
+        
     scheduler.start()
-    app.run(port=5000)
+    app.run(port=5000, debug=True)
     
